f93c5dcb9a24c064037bb35824d63ee987e10324
tinhonline262 <63954351+tinhonline262@users.noreply.github.com>
2020-07-28 20:38:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
 
-	console.log(`PossumBot Is Now Activated`);
+	console.log(`Rainbows Is Now Activated`);
 
 	//Bot Status
-	bot.user.setActivity(`With Knives`);
+	bot.user.setActivity(`.......`);
 
 	try {
 
