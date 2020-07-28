0a31bd5f7bebc954a08c0c2fdd45be7041b6f085
tinhonline262 <63954351+tinhonline262@users.noreply.github.com>
2020-07-28 20:34:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,82 +1,75 @@
-var discord = require('discord.js');
-var fs = require('fs');
-var randomColour = require('randomcolor'); // yes, the creator of this package does not speak the real english
-var Config = require('./config.json');
+const botSettings = require("./botsettings.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const prefix = botSettings.prefix;
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
-class Bot {
-    constructor(){
-        this.servers = require('./servers.json');
-        this.discordClient = new discord.Client({sync: true});
-        
-        this.discordClient.on("ready", () => {this.initialize();});
-        
-        this.discordClient.on("message", (msg) => {this.processMessage(msg)});
-        
-        this.discordClient.login(Config.NjI0NTI1MzQwNTU0ODIxNjQz.XqpYYQ.tf7K_uC6kzWWslKN4PIfA1oPeD0);
-    }
-    
-    initialize() {
-        this.log("Connected to discord.");
-        
-        setInterval(() => {
-            this.randomizeRoleColors();
-        }, Config.randomize_delay*1000);
-    }
-    
-    processMessage(msg) {
-        if(msg.content.startsWith(">addrole")) {
-            for(var role of msg.mentions.roles.array()) {
-                msg.reply("Added " + role + " to list of rainbow roles.");
-                
-                this.addRainbowRole(msg.guild.id, role.id);
-            }
-        }
-    }
-    
-    randomizeRoleColors() {
-        for(var server in this.servers) {
-            var liveGuild = this.discordClient.guilds.get(server);
-            
-            if (!liveGuild) {
-                this.error("Guild with ID " + server+ " no longer exists or the bot has been removed from it.");
-                continue;
-            }
-            
-            for(var role of this.servers[server]) {
-                var liveRole = liveGuild.roles.get(role);
-                
-                liveRole.setColor(randomColour(), "Rainbowbot random role color randomizer.");
-            }
-        }
-    }
-    
-    addRainbowRole(guild, role) {
-        if(this.servers[guild] == undefined) {
-            this.servers[guild] = [];
-        }
-        
-        for(var existingRole of this.servers[guild]) {
-            if(existingRole == role) {
-                return "That role has already been added.";
-            }
-        }
-        
-        this.servers[guild].push(role);
-        this.saveServers();
-    } 
-    
-    saveServers() {
-        fs.writeFileSync("./servers.json", JSON.stringify(this.servers), "utf8");
-        this.log("Saved servers file.");
-    }
-    
-    log(message) {
-        console.log("\x1b[32mINFO\x1b[37m - \x1b[0m" + message);
-    }
-    
-    error(message) {
-        console.log("\x1b[31mERROR\x1b[37m - \x1b[0m" + message);
-    }
-}
+fs.readdir("./commands/", (err, files) => {
 
-var instance = new Bot();
+	if (err) {
+
+		console.error(err);
+	}
+
+	let jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+	if (jsFiles.length <= 0) {
+
+		console.error("No commands found.");
+		return;
+
+	}
+
+	console.log("Loading Commands...");
+
+	jsFiles.forEach((f, i) => {
+
+		let props = require(`./commands/${f}`);
+		bot.commands.set(props.help.name, props);
+
+	});
+});
+
+bot.on("ready", async () => {
+
+	console.log(`PossumBot Is Now Activated`);
+
+	//Bot Status
+	bot.user.setActivity(`With Knives`);
+
+	try {
+
+		//Generates a invite link in the console...
+		let link = await bot.generateInvite(["ADMINISTRATOR"]);
+		console.log(link);
+
+	} catch(e) {
+
+		console.log(e.stack);
+
+	}
+});
+
+bot.on("message", async message => { 
+
+	if (message.author.bot) {
+
+        return;
+
+    }
+
+	let messageArray = message.content.split(/\s+/g);
+	let command = messageArray[0];
+	let args = messageArray.slice(1);
+
+	let cmd = bot.commands.get(command.slice(prefix.length));
+
+	if (cmd) {
+
+		cmd.run(bot, message, args);
+
+	}
+});
+
+bot.login(botSettings.token);
